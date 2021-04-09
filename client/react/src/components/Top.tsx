839f2596e26317cc1cff71aec4076970664d01a5
yyamada12 <12yacropolisy@gmail.com>
2021-04-09 09:04:18 +0900

    add style for top view

--- client/react/src/components/Top.tsx
+++ client/react/src/components/Top.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 import { useUserContext } from 'contexts/UserStore'
 
+import 'style/top.css'
+
 const Top: React.FC = () => {
   const { userId } = useUserContext()
 
   return (
     <React.Fragment>
-      <h1>Top</h1>
-      <br />
-      userId: {userId}
+      <h1 className="topTitle">
+        Login Smaple App
+      </h1>
+      <p className="topText">
+        あなたのIDは {userId} です！ 
+      </p>
     </React.Fragment>
   )
 }
